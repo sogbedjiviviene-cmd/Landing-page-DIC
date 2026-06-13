6a3c3074fa4439fad96933557baadc2f82c23c80
Wasfade <wasfadetonoukoin@gmail.com>
2026-06-13 09:16:38 +0100

    --no-edit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- Note:
+ Nous allons nous bassés sur le logo pour dégager une pallette
+l'objectif de ta Landing Page est de capter immédiatement 
+l'attention des étudiants et Visiteurs, de légitimer nos actions et 
+de pousser à l'engagement (adhésion ou participation aux 
+événements). Visuellement, le site doit se structurer 
+en une seule page fluide découpée en 5 sections clés :
+ une bannière d'accueil percutante ( Hero Section ) avec 
+ le logo de la DIC, une courte présentation et un bouton d'action 
+ principal (ex : "Nous rejoignons") ; une rubrique "Qui sommes-nous ?" 
+ présentant concrètement la vision, les valeurs et les membres du bureau (Mr Ricardo, Mourchid, Colombe, Ezechiel); 
+ une zone "No"Projets" mettant en avant vos événements phares, 
+ formations ou ateliers ; une section d'actualités ou de témoignages pour montrer 
+ la dynamique du club ; et enfin, un formulaire de contact 
+ simplifié et les liens vers vos réseaux sociaux.-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing - Page - DIC</title>
+</head>
+
+<body>
+    <header>
+        <!-- AHmas & Fidelia -->
+    </header>
+    <main>
+        <!-- Viviène, Mourchid, Junior -->
+    </main>
+    <footer>
+        <!-- Alban, Tychique, Wasfade -->
+    </footer>
+</body>
+
+</html>
